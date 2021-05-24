7e733b3d755683abe483d03cdfe33d5b04822cfd
swetha-r210802 <83578769+swetha-r210802@users.noreply.github.com>
2021-05-24 16:37:28 +0530

    cabana

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+@app.route('/register')
+def about():
+    return "About Page"
+
+if __name__=="__main__":
+    app.run(debug=True)
